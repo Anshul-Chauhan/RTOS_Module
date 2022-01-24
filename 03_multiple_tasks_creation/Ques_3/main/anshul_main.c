b3db534a27674efbecbea00172273637c74c794f
Anshul <anshulchauhan1807@gmail.com>
2022-01-25 00:00:10 +0530

    Creating an init task which further creates two another tasks with different priorities

--- 03_multiple_tasks_creation/Ques_3/main/anshul_main.c
+++ 03_multiple_tasks_creation/Ques_3/main/anshul_main.c
@@ -5,14 +5,6 @@
 
 TaskHandle_t xHandleTask, xHandleTask1, xHandleTask2;
 
-void init_task(void *pvParameters)
-{
-    printf("Initial task: Priority = 8\n");
-    xTaskCreate(Task1, "Task1", 1024, NULL, 5, &xHandleTask1);
-    xTaskCreate(Task2, "Task2", 1024, NULL, 7, &xHandleTask2); 
-    printf("Initial task is finishing after creating two tasks with same priorities.\n");
-}
-
 void Task1(void *pvParameters)
 {
     while(1)
@@ -27,6 +19,14 @@ void Task2(void *pvParameters)
     {
         printf("Task2: Priority = 7\n");
     }
+}
+
+void init_task(void *pvParameters)
+{
+    printf("Initial task: Priority = 8\n");
+    xTaskCreate(Task1, "Task1", 1024, NULL, 5, &xHandleTask1);
+    xTaskCreate(Task2, "Task2", 1024, NULL, 7, &xHandleTask2); 
+    printf("Initial task is finishing after creating two tasks with same priorities.\n");
 }
 
 void app_main()
